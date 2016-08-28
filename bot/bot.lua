662cbfefd4cdcd4ab82f23b3ac723a9567351cb6
rasool707 <r.ahmadifar.1377@gmail.com>
2016-08-28 21:03:28 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {185787155},
+    sudo_users = {205131665},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
